470091d9cea738502de5e1c5947330f1693a7a15
GPegel <gpegel@gmail.com>
2015-12-26 23:07:19 +0100

    Now you can enter a domainname

--- whois.sh
+++ whois.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-whois google.com | grep "Domain Name\|Registrant Organization\|Registrant Street\|Registrant City"
+echo "Please, enter a domain name like \"google.com"\"
+read DOMAINNAME
+whois $DOMAINNAME | grep "Domain Name\|Registrant Organization\|Registrant Street\|Registrant City"
